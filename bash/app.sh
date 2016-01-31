1c22ba7a65138b708f91b04d6e898e2835692be4
inokappa <inokara@gmail.com>
2016-02-01 00:01:21 +0900

    logger を追加

--- bash/app.sh
+++ bash/app.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 #
-. ./_function.sh
+. /app/_function.sh
+
+# ログ
+function mylogger() {
+  DATE=`date +'%Y-%m-%d %H:%M:%S'`
+  echo "${DATE} ${1} : ${2}"
+}
 
 # メッセージの取得
 function get_sqs_message() {
+  mylogger "INFO" "Get SQS Message."
   aws sqs receive-message --queue-url ${QUEUE_URL} > MESSAGE
 }
 
 # メッセージを削除
 function delete_sqs_message() {
+  mylogger "INFO" "Delete SQS Message."
   aws sqs delete-message --queue-url ${QUEUE_URL} --receipt-handle ${1}
 }
 
 # オブジェクトの取得
 function get_object_from_s3() {
-  echo "aws s3 cp s3://${1}/${2} ./images/"
+  mylogger "INFO" "execute: aws s3 cp s3://${1}/${2} ./images/"
   aws s3 cp s3://${1}/${2} ./images/
+}
+
+# オブジェクト名に空白が含まれている場合の処理
+function white_space_rename() {
+  rename -f 's/\+/\ /g' ${1}
 }
 
 # 後処理
 function teardown() {
+  mylogger "INFO" "teardown"
   # オブジェクトの取得が成功したら ReceiptHandle を利用してメッセージを削除
   delete_sqs_message ${1}
   # ダウンロード済みのメッセージを削除
@@ -33,12 +47,14 @@ do
   get_sqs_message
   
   if [ -s MESSAGE ];then
-    cat MESSAGE
     # バケット名の取得
+    mylogger "INFO" "Get Bucket Name"
     BUCKET_NAME=`cat MESSAGE | jq -r '.Messages[].Body' | jq -r '.Records[].s3.bucket.name'`
     # オブジェクト名の取得
+    mylogger "INFO" "Get Object Name"
     OBJECT_KEY_NAME=`cat MESSAGE | jq -r '.Messages[].Body' | jq -r '.Records[].s3.object.key'`
     # ReceiptHandle の取得
+    mylogger "INFO" "Get Receipt handle"
     SQS_RECEIPT_HANDLE=`cat MESSAGE | jq -r '.Messages[].ReceiptHandle'`
   
     # 複数オブジェクトが同時にアップロードされた際にスラッシュで終わるオブジェクト名がメッセージに含まれるので...
@@ -52,7 +68,7 @@ do
       teardown ${SQS_RECEIPT_HANDLE}
     fi
   else
-    echo "Message not received."
+    mylogger "INFO" "Message not received."
     rm MESSAGE
   fi
 
